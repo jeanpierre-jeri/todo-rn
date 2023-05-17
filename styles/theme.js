e56fa2e4a6141238a49e72a02ab7787d5ce4b2fb
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-17 01:06:26 -0500

    feat: add colors to theme

--- styles/theme.js
+++ styles/theme.js
@@ -1,4 +1,11 @@
 export const lightTheme = {
   background: 'hsl(236, 33%, 92%)',
-  light: 'hsl(0, 0%, 98%)'
+  light: 'hsl(0, 0%, 98%)',
+  lightGray: 'hsl(233, 11%, 84%)',
+  text: 'hsl(235, 19%, 35%)'
+}
+
+export const colors = {
+  primaryGradient: 'hsl(192, 100%, 67%)',
+  secondaryGradient: 'hsl(280, 87%, 65%)'
 }
