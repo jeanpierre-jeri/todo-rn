89b3e6b5b1271bb1ee8878e93f8eb7400bec6197
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-17 01:05:47 -0500

    feat: add Icons

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -2,6 +2,18 @@ import Svg, { Path } from 'react-native-svg'
 
 export function MoonIcon () {
   return (
-    <Svg width='21' height='21' viewBox='0 0 26 26'><Path fill='#FFF' fill-rule='evenodd' d='M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z' /></Svg>
+    <Svg width='20' height='20' viewBox='0 0 26 26'><Path fill='#FFF' fillRule='evenodd' d='M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z' /></Svg>
+  )
+}
+
+export function CheckIcon () {
+  return (
+    <Svg width='11' height='9' viewBox='0 0 11 9'><Path fill='none' stroke='#FFF' strokeWidth='2' d='M1 4.304L3.696 7l6-6' /></Svg>
+  )
+}
+
+export function CrossIcon () {
+  return (
+    <Svg width='12' height='12' viewBox='0 0 18 18'><Path fill='#494C6B' fillRule='evenodd' d='M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z' /></Svg>
   )
 }
