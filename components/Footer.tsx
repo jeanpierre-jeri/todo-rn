96202b605b5830aa5eb184a30cfd424b9ed51e7f
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-18 00:44:13 -0500

    feat: add footer for filter and clear taks

---
+++ components/Footer.tsx
@@ -0,0 +1,57 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { lightTheme } from '../styles/theme'
+import { useTasksStore } from '../store/tasks.store'
+import { useState } from 'react'
+import { FilterType } from '../types'
+import { FilterTask } from './FilterTask'
+
+const filterTypes: FilterType[] = ['all', 'active', 'completed']
+
+export function Footer () {
+  const [activeFilter, setActiveFilter] = useState<FilterType>('all')
+  const tasks = useTasksStore(state => state.tasks)
+  const tasksLeft = tasks.filter((task) => !task.completed).length
+
+  const clearCompletedTasks = useTasksStore(state => state.clearCompletedTasks)
+  const filterTasks = useTasksStore(state => state.filterTasks)
+
+  const handleFilter = (filter: FilterType) => {
+    filterTasks(filter)
+    setActiveFilter(filter)
+  }
+  return (
+    <View>
+      <View style={styles.info}>
+        <Text>{tasksLeft} {tasksLeft === 1 ? 'item' : 'items'} left</Text>
+        <Pressable onPress={clearCompletedTasks}>
+          <Text>Clear Completed</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.filters}>
+        {filterTypes.map((filter) => (
+          <FilterTask key={filter} activeFilter={activeFilter} filter={filter} handleFilter={handleFilter} />
+        ))}
+      </View>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  info: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: lightTheme.light,
+    color: lightTheme.lightGray,
+    paddingHorizontal: 20,
+    paddingVertical: 16
+  },
+  filters: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: lightTheme.light,
+    color: lightTheme.lightGray,
+    marginTop: 16
+  }
+
+})
