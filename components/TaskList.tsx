1b3d5b10b9a3b71d39811c59f4bba05693fb2727
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-19 03:07:24 -0500

    feat: get colors from theme context

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,7 +1,6 @@
 import { FlatList, type ListRenderItemInfo, StyleSheet } from 'react-native'
 import { type Task } from '../types'
 import { TaskItem } from './TaskItem'
-import { lightTheme } from '../styles/theme'
 import { useTasksStore } from '../store/tasks.store'
 
 export function TaskList () {
@@ -14,13 +13,12 @@ export function TaskList () {
   return (
     <FlatList style={styles.list} data={filteredTasks} renderItem={renderItem} keyExtractor={item => item.id} />
   )
-};
+}
 
 const styles = StyleSheet.create({
   list: {
     borderRadius: 3,
     overflow: 'hidden',
-    backgroundColor: lightTheme.light,
     marginTop: 16
   }
 })
