3d717727741fb8c9a6c7a3f73963ed0d6a9877c0
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-19 03:06:52 -0500

    feat: add sun icon

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,19 +1,26 @@
-import Svg, { Path } from 'react-native-svg'
+import { type ColorValue } from 'react-native'
+import Svg, { Path, SvgProps } from 'react-native-svg'
 
-export function MoonIcon () {
+export function MoonIcon (props: SvgProps) {
   return (
-    <Svg width='20' height='20' viewBox='0 0 26 26'><Path fill='#FFF' fillRule='evenodd' d='M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z' /></Svg>
+    <Svg {...props} width='22' height='22' viewBox='0 0 26 26'><Path fill='#FFF' fillRule='evenodd' d='M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 22.18 0 13S5.82 0 13 0z' /></Svg>
+  )
+}
+
+export function SunIcon (props: SvgProps) {
+  return (
+    <Svg {...props} width='22' height='22' viewBox='0 0 26 26'><Path fill='#FFF' fillRule='evenodd' d='M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z' /></Svg>
   )
 }
 
 export function CheckIcon () {
   return (
-    <Svg width='11' height='9' viewBox='0 0 11 9'><Path fill='none' stroke='#FFF' strokeWidth='2' d='M1 4.304L3.696 7l6-6' /></Svg>
+    <Svg width='11' height='9' viewBox='0 0 11 9'><Path fill='none' stroke='#fff' strokeWidth='2' d='M1 4.304L3.696 7l6-6' /></Svg>
   )
 }
 
-export function CrossIcon () {
+export function CrossIcon ({ fill = '#494C6B' }: { fill: ColorValue }) {
   return (
-    <Svg width='12' height='12' viewBox='0 0 18 18'><Path fill='#494C6B' fillRule='evenodd' d='M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z' /></Svg>
+    <Svg width='12' height='12' viewBox='0 0 18 18'><Path fill={fill} fillRule='evenodd' d='M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z' /></Svg>
   )
 }
