e5b51bdb30233fb725fb9e842eec2bf6048c205c
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-20 20:34:31 -0500

    styles: add dark mode and styles

--- components/FilterTask.tsx
+++ components/FilterTask.tsx
@@ -1,6 +1,7 @@
 import { Pressable, StyleSheet, Text } from 'react-native'
 import { FilterType } from '../types'
 import { colors } from '../styles/theme'
+import { useTheme } from '../context/theme.context'
 
 interface Props {
   activeFilter: FilterType
@@ -9,9 +10,11 @@ interface Props {
 }
 
 export function FilterTask ({ activeFilter, handleFilter, filter }: Props) {
+  const { theme } = useTheme()
+
   return (
     <Pressable onPress={() => handleFilter(filter)}>
-      <Text style={[styles.text, activeFilter === filter && styles.activeFilter]}>{filter}</Text>
+      <Text style={[styles.text, { color: theme.text }, activeFilter === filter && styles.activeFilter]}>{filter}</Text>
     </Pressable>
   )
 };
@@ -20,7 +23,8 @@ const styles = StyleSheet.create({
   text: {
     paddingVertical: 16,
     paddingHorizontal: 8,
-    textTransform: 'capitalize'
+    textTransform: 'capitalize',
+    fontWeight: '500'
   },
   activeFilter: {
     color: colors.blue
