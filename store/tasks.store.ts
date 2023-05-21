50b56fe4edea539725ab8f32ff1be54fe0bd0e1a
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-20 20:34:07 -0500

    style: remove initialTasks

--- store/tasks.store.ts
+++ store/tasks.store.ts
@@ -1,46 +1,12 @@
 import { create } from 'zustand'
 import { FilterType, Task } from '../types'
 import * as Crypto from 'expo-crypto'
-
-const initialTasks = [
-  {
-    id: Crypto.randomUUID(),
-    title: 'I have to be the best develoepr in the world and rule the world with javascript and typescript',
-    completed: true
-
-  },
-  {
-    id: Crypto.randomUUID(),
-    title: 'Complete online JavaScript course 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16',
-    completed: true
-
-  },
-  {
-    id: Crypto.randomUUID(),
-    title: 'Jog around the park 3x',
-    completed: false
-
-  },
-  {
-    id: Crypto.randomUUID(),
-    title: '10 minutes meditation',
-    completed: false
-  },
-  {
-    id: Crypto.randomUUID(),
-    title: 'Read for 1 hour',
-    completed: true
-  },
-  {
-    id: Crypto.randomUUID(),
-    title: 'Pick up groceries',
-    completed: false
-  }
-]
+import { storeTasks } from '../utils/storage'
 
 interface TaskStore {
   tasks: Task[]
   filteredTasks: Task[]
+  addInitialTasks: (tasks: Task[]) => void
   addTask: (title: string) => void
   removeTask: (id: string) => void
   toggleTask: (id: string) => void
@@ -49,8 +15,8 @@ interface TaskStore {
 }
 
 export const useTasksStore = create<TaskStore>()((set, get) => ({
-  tasks: initialTasks,
-  filteredTasks: initialTasks,
+  tasks: [],
+  filteredTasks: [],
   addTask: (title) => {
     const tasks = [
       {
@@ -61,10 +27,16 @@ export const useTasksStore = create<TaskStore>()((set, get) => ({
       ...get().tasks
     ]
 
+    void storeTasks(tasks)
+
+    set({ tasks, filteredTasks: tasks })
+  },
+  addInitialTasks: (tasks) => {
     set({ tasks, filteredTasks: tasks })
   },
   removeTask: (id) => {
     const tasks = get().tasks.filter((task) => task.id !== id)
+    void storeTasks(tasks)
     set({ tasks, filteredTasks: tasks })
   },
   toggleTask: (id) => {
@@ -78,10 +50,12 @@ export const useTasksStore = create<TaskStore>()((set, get) => ({
       return task
     })
 
+    void storeTasks(tasks)
     set({ tasks, filteredTasks: tasks })
   },
   clearCompletedTasks: () => {
     const tasks = get().tasks.filter((task) => !task.completed)
+    void storeTasks(tasks)
     set({ tasks, filteredTasks: tasks })
   },
   filterTasks: (filter) => {
